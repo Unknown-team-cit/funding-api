a269a2d745221c0894256f8d7f61e01b9288c16b
naveen.kumar <naveen.kumar@gogosoon.com>
2021-03-11 00:20:00 +0530

    Adds routes for get and post method

---
+++ backend/routes/route.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const Startup = require("../schema/Startup");
+router.get("/work", async (req, res) => {
+  const startups = await Startup.find();
+  res.json(startups);
+});
+
+router.post("/addstartup", async (req, res) => {
+  console.log("body", req.body);
+  await new Startup(req.body).save((err, data) => {
+    if (err) {
+      res.send("error", err);
+    } else {
+      res.json({
+        status: "success",
+      });
+    }
+  });
+});
+
+module.exports = router;
+
+// {
+// 	"name_of_company":"name com",
+// 	"name_of_owner":"naveen",
+// 	"work_field":"ai",
+// 	"website_link":"web link",
+// 	"linkedin":"linked link",
+// 	"ph_number":"12345678",
+// 	"email_id":"a@gmail.com",
+// 	"password":"pass",
+// 	"ppt_link":"pp link",
+// 	"country":"india",
+// 	"state":"tamil nadu",
+// 	"city":"chennai",
+// 	"domain":"cs",
+// 	"post_content":"ps"
+// }
+
+// router.post("/addstartup", (req, res) => {
+//     const companyName = "hai"
+//     const ownerName = req.body.name_of_owner;
+//     const work = req.body.work_field;
+//     const website = req.body.website_link;
+//     const linkedin = req.body.linkedin;
+//     const ph_number = req.body.ph_number;
+//     const email = req.body.email_id;
+//     const password = req.body.password;
+//     const Ppt = req.body.ppt_link;
+//     const country = req.body.country;
+//     const state = req.body.state;
+//     const city = req.body.city;
+//     const domain = req.body.domain;
+//     const post = req.body.post_content;
+//     const startup = new Startup({
+//       companyName,
+//       ownerName,
+//       work,
+//       website,
+//       linkedin,
+//       ph_number,
+//       email,
+//       password,
+//       Ppt,
+//       country,
+//       state,
+//       city,
+//       domain,
+//       post,
+//     });
+//     startup
+//       .save()
+//       .then(() => {
+//         res.json({
+//           status: "success",
+//         });
+//       })
+//       .catch((err) => {
+//         console.log("error", err);
+//       });
+//   });
