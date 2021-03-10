d384dcd51719c54e18c359d2d5b980974c0642bf
naveen.kumar <naveen.kumar@gogosoon.com>
2021-03-10 07:42:43 +0530

    Adds a simple get method

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("hai");
+const express = require("express");
+const app = express();
+const PORT = 8000;
+app.get("/", (req, res) => {
+  res.send("get req worked");
+});
+app.listen(PORT, () => {
+  console.log("server is running in port", PORT);
+});
