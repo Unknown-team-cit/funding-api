59d442d6ed370b48dfbf3f1d30a3785e3e450a0f
naveen.kumar <naveen.kumar@gogosoon.com>
2021-03-19 21:17:17 +0530

    Makes the fields as required

--- backend/schema/Startup.js
+++ backend/schema/Startup.js
@@ -4,45 +4,57 @@ const StartupSchema = new mongoose.Schema(
   {
     name_of_company: {
       type: String,
+      required: true
     },
     name_of_owner: {
       type: String,
+      required: true
     },
     work_field: {
       type: String,
+      required: true
     },
     website_link: {
       type: String,
     },
     linkedin: {
       type: String,
+      required: true
     },
     ph_number: {
       type: String,
+      required: true
     },
     email_id: {
       type: String,
+      required: true
     },
     password: {
       type: String,
+      required: true
     },
     ppt_link: {
       type: String,
     },
     country: {
       type: String,
+      required: true
     },
     state: {
       type: String,
+      required: true
     },
     city: {
       type: String,
+      required: true
     },
     domain: {
       type: String,
+      required: true
     },
     post_content: {
       type: String,
+      required: true
     },
     messages: [
       {
